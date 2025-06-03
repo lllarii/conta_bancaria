0a4f7f98021fc346a90b00d8d4157e37590df766
unknown <larissateixeira998@gmail.com>
2025-06-03 13:47:37 -0300

    Criando a classe Conta

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,24 @@
 import readlinesync = require ("readline-sync");
 import {colors} from './scr/util/Colors';
+import { Conta } from "./scr/model/Conta";
 
 export function main ()
 {
     let opcao: number;
+
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
+
+    const conta2: Conta = new Conta(2, 132, 2, "Thiago", 5000);
+    conta2.visualizar();
+    conta2.sacar(500);
+    conta2.visualizar();
+    conta2.depositar(1000);
+    conta2.visualizar();
 
     while (true)
     {
